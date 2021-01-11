83b85c39b90daac7c432a00c9ea94703b03eb6ac
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-11 19:41:54 -0300

    criação das entidades Empresa e Banco

--- src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -19,9 +19,13 @@ public class NovaEmpresaServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando Nova Empresa");
 		String nomeEmpresa = request.getParameter("nome");
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>Empresa "+ nomeEmpresa + " cadastrada com Sucesso</body></html>");
 		
